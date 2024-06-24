129a8f24c273cada7973c5c62b6270ce664bb878
Max Nurzia <max@mmn.sh>
2024-06-24 14:13:45 -0400

    make headings links too
    Signed-off-by: Max Nurzia <max@mmn.sh>

--- tools/scripts/make_docs.py
+++ tools/scripts/make_docs.py
@@ -331,7 +331,7 @@ def _doc_api(args, lines: list[str]) -> int:
     link_names: dict[str, str] = {}
 
     def heading(s: str, ids: list[str], lvl: int) -> str:
-        return f'<h{lvl} id="{ids[0]}">{s}</h{lvl}>'
+        return f'<h{lvl} id="{ids[0]}"><a href="#{ids[0]}">{s}</a></h{lvl}>'
 
     def ref(s: str, name: str) -> str:
         return f'<a href="#{link_names[name]}">{s}</a>'
